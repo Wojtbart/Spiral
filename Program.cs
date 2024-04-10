2a82076ad04b39dfd80325de7c60a4c722ab61d9
Wojtbart <wsekowski@poczta.onet.pl>
2024-04-10 18:07:26 +0200

    Ostatnie poprawki

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
-﻿
-namespace Spirala
+﻿namespace Spiral
 {
     class Program
     {
-        public static void Main(string[] args)
-        {
-            int v = 7;
-            GenerateSpiral(v);
-        }
-
         static int[,] GenerateSpiral(int n)
         {
+            if (n % 2 == 0 || n<=0) 
+                throw new Exception("Podano niewłaściwą liczbę! To zabronione! Poprawne liczby to dodatnie liczby nieparzyste!");
+
             int[,] spiral = new int[n, n];
-            int counter =0;
+            int counter = 0;
+
             FillSpiral(spiral, 0, 0, n - 1, n - 1, 1, counter);
+
+            //WYPISYWANIE WYNIKU
+            Console.WriteLine("Rysuję spiralę!\n");
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
@@ -26,42 +26,58 @@ namespace Spirala
                 Console.WriteLine();
             }
             Console.WriteLine();
+
             return spiral;
         }
 
         static void FillSpiral(int[,] spiral, int top, int left, int bottom, int right, int value, int counter)
         {
-            if (counter % 2 == 0) value = 1;
-            else value = 0;
+            if (counter % 2 == 1) value = 0;
+            else value = 1;
+
             if (top > bottom || left > right)
                 return;
 
             for (int i = left; i <= right; i++)
+            {
                 spiral[top, i] = value;
-               // value++;
+            }
 
             for (int i = top + 1; i <= bottom; i++)
+            {
                 spiral[i, right] = value;
-               // value++;
+            }
 
             if (top != bottom)
             {
                 for (int i = right - 1; i >= left; i--)
+                {
                     spiral[bottom, i] = value;
-                   // value++;
+                }
+
             }
 
             if (left != right)
             {
                 for (int i = bottom - 1; i > top; i--)
                 {
-                    if (i == top + 1) continue;
+                    if (i == 1) continue;
+                    if (i % 2 ==1 && left % 2 == 0 && i - left == 1 ) continue;
+                    if(i == top + 1) value = 1;
+                    
                     spiral[i, left] = value;
-                    //value++;
                 }
+
             }
             counter++;
+
             FillSpiral(spiral, top + 1, left + 1, bottom - 1, right - 1, value, counter);
+        }
+
+        public static void Main(string[] args)
+        {
+            int v = 9;
+            GenerateSpiral(v);
         }
     }
 }
